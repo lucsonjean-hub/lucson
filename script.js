b0de06f449dde05980e6b33549ce8d148788c5bd
lucsonjean-hub <lucson.jean@escola.pr.gov.br>
2026-06-12 10:43:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+document.getElementById("btnSaibaMais").addEventListener("click", () => {
+    alert(
+        "O Paraná está entre os estados líderes do agronegócio brasileiro, com destaque para soja, milho, carnes e cooperativismo."
+    );
+});
+
+const numeros = document.querySelectorAll(".numero");
+
+numeros.forEach(numero => {
+    numero.addEventListener("mouseover", () => {
+        numero.style.color = "#ff9800";
+    });
+
+    numero.addEventListener("mouseout", () => {
+        numero.style.color = "#1b5e20";
+    });
+});
